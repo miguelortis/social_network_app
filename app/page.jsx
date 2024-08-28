4bce2b4b6f06ac715d45ed4e512ad74d6c6291f3
Pedro Arguelles <pedroarguelles1996@gmail.com>
2024-08-28 15:35:22 -0400

    minor change

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,20 @@
 "use client";
 
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import "./styles.css";
 
 export default function Home() {
+  const router = useRouter();
+
+  const redirectToLogin = () => {
+    router.push("/login");
+  };
+
+  useEffect(() => {
+    redirectToLogin();
+  }, []);
+
   return (
     <>
       <h1>HOME</h1>
