5ff261c126d10cf020a7aaa61e98a2b97ab4332c
Pedro Arguelles <pedroarguelles1996@gmail.com>
2024-09-29 16:54:51 -0400

    minor changes

--- components/UserProfilePic/UserProfilePic.jsx
+++ components/UserProfilePic/UserProfilePic.jsx
@@ -1,20 +1,20 @@
 import Avatar from '../Avatar/Avatar'
 import Skeleton from '../Skeleton/Skeleton'
-import { useLoggedUserStore } from '../../store/logged-user-store'
+import { useAuth } from '../../hooks/stores/useAuth'
 
 export default function UserProfilePic() {
-  const loggedUserData = useLoggedUserStore((state) => state.loggedUserData)
+  const { loggedUser } = useAuth()
 
   return (
     <div className='flex items-center pt-4 pb-4'>
       <div className='flex min-w-0 gap-x-4'>
-        <Avatar src={loggedUserData?.profilePic} className='h-12 w-12' />
+        <Avatar src={loggedUser?.avatar} className='h-12 w-12' />
       </div>
       <div className='shrink-0 sm:flex sm:flex-col px-2'>
         <div className='min-w-0 flex-auto'>
-          {loggedUserData?.name && loggedUserData?.lastName ? (
+          {loggedUser?.name && loggedUser?.lastName ? (
             <p className='text-sm font-semibold leading-6 text-gray-900'>
-              {loggedUserData?.name || ''} {loggedUserData?.lastName || ''}
+              {loggedUser?.name || ''} {loggedUser?.lastName || ''}
             </p>
           ) : (
             <Skeleton variant='text' width={120} height={20} />
