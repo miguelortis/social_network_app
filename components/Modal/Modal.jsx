fc4152d379c66f149a537d292631c448276bb9f6
Pedro Arguelles <pedroarguelles1996@gmail.com>
2024-11-16 13:02:03 -0400

    minor changes

--- components/Modal/Modal.jsx
+++ components/Modal/Modal.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 
 const Modal = ({ children, open, onClose }) => {
